b5cfd3d1ebfa89a9bfbc574bd2a971ea31354463
Yueyu <gaoboyuan8@qq.com>
2018-11-13 16:49:07 +0800

    Update

--- D3D11Hook/custom_present.cpp
+++ D3D11Hook/custom_present.cpp
@@ -9,27 +9,6 @@
 #define C(x) x
 #endif
 
-//https://stackoverflow.com/a/13438807
-BOOL CheckWindowsVersion(DWORD dwMajor, DWORD dwMinor, DWORD dwBuild)
-{
-	// Initialize the OSVERSIONINFOEX structure.
-	OSVERSIONINFOEX osvi;
-	ZeroMemory(&osvi, sizeof(OSVERSIONINFOEX));
-	osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
-	osvi.dwMajorVersion = dwMajor;
-	osvi.dwMinorVersion = dwMinor;
-	osvi.dwBuildNumber = dwBuild;
-
-	// Initialize the condition mask.
-	DWORDLONG dwlConditionMask = 0;
-	VER_SET_CONDITION(dwlConditionMask, VER_MAJORVERSION, VER_GREATER_EQUAL);
-	VER_SET_CONDITION(dwlConditionMask, VER_MINORVERSION, VER_GREATER_EQUAL);
-	VER_SET_CONDITION(dwlConditionMask, VER_BUILDNUMBER, VER_GREATER_EQUAL);
-
-	// Perform the test.
-	return VerifyVersionInfo(&osvi, VER_MAJORVERSION | VER_MINORVERSION | VER_BUILDNUMBER, dwlConditionMask);
-}
-
 class D2DCustomPresent
 {
 private:
@@ -38,11 +17,11 @@ private:
 	DirectX::SimpleMath::Vector2 textpos, textanchorpos;
 	ResLoader resloader;
 	ID3D11DeviceContext *pContext;
-	NOTIFYICONDATA nid;
+	unsigned t1, t2, fcount;
+	TCHAR fpstext[16];
 public:
-	D2DCustomPresent():pContext(nullptr)
+	D2DCustomPresent():pContext(nullptr),t1(0),t2(0),fcount(0)
 	{
-		nid.hWnd = NULL;
 	}
 	D2DCustomPresent(D2DCustomPresent &&other)
 	{
@@ -52,41 +31,10 @@ public:
 		textanchorpos = std::move(other.textanchorpos);
 		resloader = std::move(other.resloader);
 		pContext = std::move(other.pContext);
-		nid = std::move(other.nid);
 	}
 	~D2DCustomPresent()
 	{
 		Uninit();
-		if (nid.hWnd)
-			Shell_NotifyIcon(NIM_DELETE, &nid);
-	}
-	void ShowTrayBalloon(IDXGISwapChain *pSC)
-	{
-		if (nid.hWnd)
-			Shell_NotifyIcon(NIM_DELETE, &nid);
-		DXGI_SWAP_CHAIN_DESC desc;
-		pSC->GetDesc(&desc);
-		nid.hWnd = desc.OutputWindow;
-		if (CheckWindowsVersion(6, 0, 6))
-			nid.cbSize = sizeof(NOTIFYICONDATA);
-		else if (CheckWindowsVersion(6, 0, 0))
-			nid.cbSize = NOTIFYICONDATA_V3_SIZE;
-		else if (CheckWindowsVersion(5, 0, 0))
-			nid.cbSize = NOTIFYICONDATA_V2_SIZE;
-		else
-			nid.cbSize = NOTIFYICONDATA_V1_SIZE;
-		nid.uID = 2000;
-		nid.hIcon = (HICON)GetClassLongPtr(nid.hWnd, GCLP_HICON);
-		nid.uFlags = NIF_ICON | NIF_TIP | NIF_INFO;
-		nid.uVersion = NOTIFYICON_VERSION;
-		nid.uTimeout = 5000;
-		TCHAR text[256];
-		GetWindowText(nid.hWnd, text, ARRAYSIZE(text));
-		wsprintf(nid.szTip, TEXT("D3D11Hook [%s]"), text);
-		lstrcpy(nid.szInfoTitle, TEXT("D3D11Hook 加载成功。"));
-		wsprintf(nid.szInfo, TEXT("SwapChain: %p\nHWND: %p"), pSC, nid.hWnd);
-		nid.dwInfoFlags = NIIF_INFO;
-		Shell_NotifyIcon(NIM_ADD, &nid);
 	}
 	BOOL Init(IDXGISwapChain*pSC)
 	{
@@ -99,7 +47,6 @@ public:
 		C(resloader.LoadFontFromSystem(spriteFont, 1024, 1024, L"宋体", 48, D2D1::ColorF(D2D1::ColorF::White), DWRITE_FONT_WEIGHT_REGULAR));
 		textpos.x = textpos.y = 0.0f;
 		textanchorpos.x = textanchorpos.y = 0.0f;
-		ShowTrayBalloon(pSC);
 		return TRUE;
 	}
 	void Uninit()
@@ -109,8 +56,6 @@ public:
 	}
 	void Draw()
 	{
-		static unsigned t1 = 0, t2 = 0, fcount = 0;
-		static TCHAR fpstext[16];
 		if (fcount--==0)
 		{
 			fcount = 60;
